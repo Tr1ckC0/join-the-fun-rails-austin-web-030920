65dd755e835bd4be69f025a317d6c9bf2bd5e458
Tr1ckC0 <trick.co@yahoo.com>
2020-04-06 12:12:51 -0500

    Done.

--- db/migrate/20140212204456_create_taxis.rb
+++ db/migrate/20140212204456_create_taxis.rb
@@ -1,6 +1,7 @@
-class CreateTaxis < ActiveRecord::Migration
+class CreateTaxis < ActiveRecord::Migration[5.0]
   def change
     create_table :taxis do |t|
+      t.integer :taxi_number
       t.timestamps null: false
     end
   end
